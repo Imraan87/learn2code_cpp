98e860b62b766e7cbf7364c129193ae36102f010
Imraan87 <imraani87@gmail.com>
2019-04-09 20:40:59 +0100

    add logfile example to repository

--- eclipse/src/main.cpp
+++ eclipse/src/main.cpp
@@ -13,7 +13,7 @@
 
 int main(){
 
-	LogFile Log = LogFile("C:\\Users\\iibrah\\Desktop\\Tester_logs\\FULL_X_4_Y_0_wafid_21_lotid_0CC38_d2018_11_28_t09_14_17.txt");
+	LogFile Log = LogFile("C:\\Users\\Imraan\\Documents\\mculogparser\\learn2code_cpp\\eclipse\\src\\log_examples\\FULL_X_4_Y_0_wafid_21_lotid_0CC38_d2018_11_28_t09_14_17.txt");
 
 	//cout << Log.outputLog() << endl << endl;
 
