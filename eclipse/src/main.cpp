7e5150d0f6c3996d34fc3af6049f5744432241dc
Imraan Ibrahim <imraani87@gmail.com>
2020-07-19 03:40:20 +0100

    requires c++17

--- eclipse/src/main.cpp
+++ eclipse/src/main.cpp
@@ -10,12 +10,15 @@
 
 #include "logfile\LogFile.h"
 #include "data_parser\pllparser.h"
+//#include <filesystem>
+//namespace fs = std::filesystem;
 
 int main(){
 
 	LogFile Log = LogFile("C:\\Users\\Imraan\\Documents\\mculogparser\\learn2code_cpp\\eclipse\\src\\log_examples\\FULL_X_4_Y_0_wafid_21_lotid_0CC38_d2018_11_28_t09_14_17.txt");
 
 	//cout << Log.outputLog() << endl << endl;
+
 
 	//cout << Log.getChipName() << endl;
 
